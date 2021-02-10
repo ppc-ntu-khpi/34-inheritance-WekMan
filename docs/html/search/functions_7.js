5cc934891db6e615180b8e0c67a55bb59aa8ea5f
WekMan <58940879+WekMan@users.noreply.github.com>
2021-02-11 01:30:59 +0200

    Add files via upload

---
+++ docs/html/search/functions_7.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['tostring_36',['toString',['../classdomain_1_1_animal.html#a1b29e1c4522c9593ac169ea0b113fa07',1,'domain.Animal.toString()'],['../classdomain_1_1_lion.html#af24aa2e2d22a0364a4ef0f7bf57aa3df',1,'domain.Lion.toString()']]]
+];
