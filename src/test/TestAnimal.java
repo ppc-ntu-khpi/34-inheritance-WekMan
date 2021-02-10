45d5a98ef444c1cc6160ecfd5530a44333f350ef
WekMan <58940879+WekMan@users.noreply.github.com>
2021-02-11 00:57:58 +0200

    Add files via upload

--- src/test/TestAnimal.java
+++ src/test/TestAnimal.java
@@ -1,9 +1,21 @@
 package test;
 import domain.Lion;
 
+
+ /**
+ * The class Test animal
+ */ 
 public class TestAnimal {
 
-    public static void main(String[] args) {
+
+/** 
+ *
+ * Main
+ *
+ * @param args  the args
+ */
+    public static void main(String[] args) { 
+
         Lion lion = new Lion("Bob");
         System.out.println(lion);
         lion.hunt();
