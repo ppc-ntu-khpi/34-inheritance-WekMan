902812298b3ab1d75eb65c0ecd956262b897d88d
WekMan <58940879+WekMan@users.noreply.github.com>
2021-02-11 12:01:47 +0200

    Add files via upload

---
+++ docs/search/all_4.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['lion_6',['Lion',['../classdomain_1_1_lion.html',1,'domain.Lion'],['../classdomain_1_1_lion.html#a58fde4e6178e34bc7300f780a23c50de',1,'domain.Lion.Lion(String name, int weight, int age, String kind)'],['../classdomain_1_1_lion.html#a3bd72092d8ecec1a3d5ff41abfab32bb',1,'domain.Lion.Lion()'],['../classdomain_1_1_lion.html#aec3411ff13a2bbb96d201fdaafcd1baa',1,'domain.Lion.Lion(String name)']]],
+  ['lion_2ejava_7',['Lion.java',['../_lion_8java.html',1,'']]]
+];
