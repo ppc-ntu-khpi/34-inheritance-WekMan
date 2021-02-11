983d280213a0998199f9a1054cf9fa22105d79a7
WekMan <58940879+WekMan@users.noreply.github.com>
2021-02-11 11:59:30 +0200

    Add files via upload

---
+++ docs/classdomain_1_1_lion.js
@@ -0,0 +1,11 @@
+var classdomain_1_1_lion =
+[
+    [ "Lion", "classdomain_1_1_lion.html#a58fde4e6178e34bc7300f780a23c50de", null ],
+    [ "Lion", "classdomain_1_1_lion.html#a3bd72092d8ecec1a3d5ff41abfab32bb", null ],
+    [ "Lion", "classdomain_1_1_lion.html#aec3411ff13a2bbb96d201fdaafcd1baa", null ],
+    [ "eat", "classdomain_1_1_lion.html#aa6c69473473fbd74051e1d375e1550c7", null ],
+    [ "hunt", "classdomain_1_1_lion.html#a32ee12463bdc01086941372fad2da01b", null ],
+    [ "play", "classdomain_1_1_lion.html#aa032c7083dd30e01df0af08650553374", null ],
+    [ "speak", "classdomain_1_1_lion.html#af3207b892b0833c5a0acbf8d466b8aeb", null ],
+    [ "toString", "classdomain_1_1_lion.html#af24aa2e2d22a0364a4ef0f7bf57aa3df", null ]
+];
---
+++ docs/hierarchy.js
@@ -0,0 +1,9 @@
+var hierarchy =
+[
+    [ "domain.Animal", "classdomain_1_1_animal.html", [
+      [ "domain.Predator", "classdomain_1_1_predator.html", [
+        [ "domain.Lion", "classdomain_1_1_lion.html", null ]
+      ] ]
+    ] ],
+    [ "test.TestAnimal", "classtest_1_1_test_animal.html", null ]
+];
